56b2d061bfa92ee666f351740f7ecad0cdd749fe
Fredy Ballesteros <fredy.ballesteros@gmail.com>
2019-06-04 00:07:56 -0500

    Actualiza Titulo

--- prodjango/webplayground/core/views.py
+++ prodjango/webplayground/core/views.py
@@ -5,11 +5,7 @@ class HomePageView(TemplateView):
     template_name = "core/home.html"
 
     def get(self, request, *args, **kwargs):
-<<<<<<< HEAD
-        return render(request, self.template_name, {'title':"DevOps Univalle Prueba rama 1"})
-=======
         return render(request, self.template_name, {'title':"DevOps Univalle Cambia Rama Leidy"})
->>>>>>> branchleidy
 
 class SamplePageView(TemplateView):
     template_name = "core/sample.html"
